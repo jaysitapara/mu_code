0b547b2daabdec44eb297c501e4a51a64fc273f2
Jayu <jaysitapara5103@gmail.com>
2024-02-21 10:58:23 +0530

    done

--- Python/index.py
+++ Python/index.py
@@ -237,3 +237,4 @@
 # ==================================================================
 
 # 13.
+# write a program to make use of map(), filter() and reduce() functions with context to lamda function.
