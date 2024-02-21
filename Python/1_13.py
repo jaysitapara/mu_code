02428f28687e6e64cef8fb2ff4e4b9b927a42ac7
Jay Sitapara <jaysitapara5103@gmail.com>
2024-02-21 16:03:37 +0530

    Update 1_13.py

--- Python/1_13.py
+++ Python/1_13.py
@@ -223,17 +223,31 @@
 # ==================================================================
 
 # 12.
-# x = 10
-# def outer():
-#     x = 20
-#     def inner():
-#         nonlocal x
-#         x = 30
-#         print("inner:", x)
-#     inner()
-#     print("outer:", x)
-# outer()
-# print("global:", x)
+#local variable
+#def local_v():
+# local variable
+#   message = 'Hello Dhruv'
+#  print('Local', message)
+#local_v()
+#print(message)
+
+# declare global variable
+#message = 'Hello Vinay'
+
+#def Global_v():
+#   print('Local', message)
+#Global_v()
+#print('Global', message)
+
+#nonlocal variables
+#def outer():
+#   message="Local"
+#  def inner():
+#     nonlocal message
+#    print("inner:",message)
+#inner()
+#print("outer:",message)
+#outer()
 # ==================================================================
 
 # 13.
